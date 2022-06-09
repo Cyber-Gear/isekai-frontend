7ab431942a020c550f41dccb3052f7f8c3ff4941
Yangx <641160771@qq.com>
2022-06-09 18:14:37 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   publicPath: "/",
   devServer: { disableHostCheck: true },
   lintOnSave: true,
-  // productionSourceMap: isProd ? false : true,
+  productionSourceMap: isProd ? false : true,
 
   chainWebpack(config) {
     config.module.rule("scss").oneOfs.store.forEach((item) => {
@@ -30,25 +30,25 @@ module.exports = {
   configureWebpack: (config) => {
     config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
     // 开启分离打包js文件
-    // config.optimization = {
-    //   runtimeChunk: "single", // 'single': 会生成一个唯一单独的runtime.js文件，就是manifest。
-    //   splitChunks: {
-    //     // 主要就是根据不同的策略来分割打包出来的bundle。
-    //     chunks: "all", // 同时分割同步和异步代码，推荐。
-    //     maxInitialRequests: Infinity, // 一个入口最大的并行请求数（字面意思，一般不建议改。）
-    //     minSize: 20000, // 按需加载时候最大的并行请求数
-    //     cacheGroups: {
-    //       // 缓存策略，默认设置了分割node_modules和公用模块。
-    //       vendor: {
-    //         test: /[\\/]node_modules[\\/]/, // 匹配规则
-    //         name(module) {
-    //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]; // 重写文件名称
-    //           return `npm.${packageName.replace("@", "")}`;
-    //         },
-    //       },
-    //     },
-    //   },
-    // };
+    config.optimization = {
+      runtimeChunk: "single", // 'single': 会生成一个唯一单独的runtime.js文件，就是manifest。
+      splitChunks: {
+        // 主要就是根据不同的策略来分割打包出来的bundle。
+        chunks: "all", // 同时分割同步和异步代码，推荐。
+        maxInitialRequests: Infinity, // 一个入口最大的并行请求数（字面意思，一般不建议改。）
+        minSize: 20000, // 按需加载时候最大的并行请求数
+        cacheGroups: {
+          // 缓存策略，默认设置了分割node_modules和公用模块。
+          vendor: {
+            test: /[\\/]node_modules[\\/]/, // 匹配规则
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]; // 重写文件名称
+              return `npm.${packageName.replace("@", "")}`;
+            },
+          },
+        },
+      },
+    };
     if (isProd) {
       Object.assign(config, {
         output: {
